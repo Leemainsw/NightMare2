6388cecacf1862b8469e25efefac1e8daf626280
jiwonan <s2018s31@e-mirim.hs.kr>
2020-06-04 23:08:57 +0900

    플레이어 이동 수정, 총 발사 구현

--- SweetHouse/Assets/Scripts/ControllerInput.cs
+++ SweetHouse/Assets/Scripts/ControllerInput.cs
@@ -5,31 +5,37 @@ using UnityEngine;
 public class ControllerInput : MonoBehaviour
 {
 
-    public int speedForward = 12; // 전진 속도
+    /*public int speedForward = 12; // 전진 속도
     public int speedSide = 6; // 옆걸음 속도
 
     private Transform tr;
     private float dirX = 0;
     private float dirZ = 0;
-    
-    // public Camera cam;
+    */
+
+    private Transform camTr;
+    public Camera cam;
     private float speed = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        tr = GetComponent<Transform>();
+         camTr = cam.GetComponent<Transform>();
         // camTr = camObj.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        MovePlayer();
+
+        if (OVRInput.Get(OVRInput.Button.PrimaryTouchpad))
+        {
+            MovePlayer();
+        }
     }
 
     // 플레이어 이동
-    void MovePlayer()
+    /*void MovePlayer()
     {
         dirX = 0; // 좌우 이동(왼쪽: -1, 오른쪽: 1)
         dirZ = 0; // 전진 후진(후진: -1, 전진: 1)
@@ -44,6 +50,7 @@ public class ControllerInput : MonoBehaviour
             {
                 if (coord.x > 0) // 오
                     dirX = +1;
+                    
                 else // 왼
                     dirX = -1;
             }
@@ -58,20 +65,18 @@ public class ControllerInput : MonoBehaviour
         }
         // 이동 방향 설정 후 이동
         
-        Vector3 moveDir = new Vector3(dirX * speedSide, 0, dirZ * speedForward);
+        Vector3 moveDir = new Vector3(dirX * speed, 0, dirZ * speed);
         transform.Translate(moveDir * Time.smoothDeltaTime);
         
+        // transform.rotation = Quaternion.LookRotation(moveDir); 원하는 방향 쳐다보기.
+
+    }*/
+
+    void MovePlayer()
+    {
+        Vector3 dir = camTr.TransformDirection(Vector3.forward);
+
+        transform.Translate(dir * speed);
     }
 
-
-    /*void MovePlayer()
-    {
-        if (OVRInput.Get(OVRInput.Button.PrimaryTouchpad))
-        {
-            Vector3 dir = camTr.TransformDirection(Vector3.forward);
-
-            transform.Translate(dir * speed);
-
-        }
-    }*/
 }
