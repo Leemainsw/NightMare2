7d33b2e9651369ce15ba3697eb1cf24b8c8cbe06
Leemainsw <s2018s15@e-mirim.hs.kr>
2020-06-05 00:28:16 +0900

    Add Zoombie Object

---
+++ SweetHouse/Assets/Scripts/ZoombieController.cs
@@ -0,0 +1,21 @@
+﻿using Oculus.Platform.Samples.VrHoops;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZoombieController : MonoBehaviour
+{
+    private Transform _transform;
+    private Transform playerTransform;
+    private NavMeshAgent nvAgent;
+
+    void Start()
+    {
+        _transform = this.gameObject.GetComponent<Transform>();
+        playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        nvAgent = this.gameObject.GetComponent<NavMeshAgent>();
+
+        nvAgent.destination = playerTransform.position;
+    }
+}
