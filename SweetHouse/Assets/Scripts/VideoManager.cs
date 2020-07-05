0987a556f594cca3dd053ce3469957b8dfb7c640
Leemainsw <s2018s15@e-mirim.hs.kr>
2020-07-05 18:59:51 +0900

    영상삽입 메인씬

---
+++ SweetHouse/Assets/Scripts/VideoManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoManager : MonoBehaviour
+{
+    private VideoPlayer m_VideoPlayer;
+    
+    void Awake()
+    {
+        m_VideoPlayer = GetComponent<VideoPlayer>();
+        m_VideoPlayer.loopPointReached += OnMovieFinished; // loopPointReached is the event for the end of the video
+    }
+    
+    void OnMovieFinished(VideoPlayer player)
+    {
+        Debug.Log("Event for movie end called");
+        player.Stop();
+
+        SceneManager.LoadScene("GameStart");
+
+    }
+}
